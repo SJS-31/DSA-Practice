ad5706e6880da95c60cce8ff15149660c710d44a
SJS-31 <rajswapnil31@gmail.com>
2023-07-18 02:51:55 +0530

    Recurssion Quick SOrt

---
+++ RecQuickSort.java
@@ -0,0 +1,59 @@
+
+public class RecQuickSort {
+
+    public static void quickSort(int []arr, int s, int e) {
+        if(s>=e)
+            return;
+
+        int p = partion(arr, s,e);
+
+        quickSort(arr, s, p-1);
+        quickSort(arr, p+1, e);
+    }
+
+    public static int partion(int []arr,int s, int e) {
+        int count =0;
+        for (int i=s+1; i<=e;i++)
+        {
+            if(arr[s]>arr[i])
+            count++;
+        }
+
+        int p= s+count;
+
+        int temp=arr[p];
+        arr[p]=arr[s];
+        arr[s]=temp;
+
+
+        while(s< p && e>p)
+        {
+            if(arr[s]>arr[p] && arr[e]<arr[p])
+            {
+                temp=arr[e];
+                arr[e]=arr[s];
+                arr[s]=temp;
+                s++;
+                e--;
+            }
+            if(arr[s]<arr[p])
+            s++;
+
+            if(arr[e]>arr[p])
+            e--;
+
+        }
+        return p;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,5,6,7,999,7};
+        
+        quickSort(arr, 0, 5);
+
+        for(int i=0; i<6;i++)
+        {
+            // System.out.println("arr");
+            System.out.print(arr[i]);
+        }
+    }
+}
